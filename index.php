2dda60d1c5d2db0228f981b121a765c836866790
LuizGustavoReis <luizgusreis@gmail.com>
2022-08-17 10:40:00 -0300

    treino

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php
-echo "Meu nome e jonny";
+
+
+
+echo "Meu nome e jonny"; //a função echo escreve qualquer texto na tela (ja e nativa)
+echo "<h1> função nativa echo</h1>";
+echo "<br>";
+echo "bem vindo";
+
+
+echo "<h4></h4>";
+$nota1 = 7.5;
+$nota2 = 8;
+$texto = "minha media e:";
+$resultado = ($nota1.$nota2)/2;
+
+$texto = $texto.$resultado;
+
+
+echo $texto;
+
+if ($resultado >= 6)
+{
+    echo "aprovado";
+}
+else if ($resultado <6 && $resultado >=2)
+{
+echo "tem chance de recuperar";
+}
+else{
+    echo "reprovado, perdido";
+}
+$contadora = 0;
+do{
+echo "<br>teste".$contadora;
+$contadora ++;
+
+}while($contadora < 10);
+//echo "<BR><BR>degub:".$resultado."<BR><BR><br>";
+
+echo "while:<br>";
+while($contadora < 150)
+{
+    if($contadora % 2 == 0){
+        echo $contadora."e numero par<br>";
+    
+    }else{
+        echo "<p>".$contadora."E numero impar</p>";
+    }
+    $contadora++;
+}
+
+CONST teste = "oi";
+for($i = 0; $i<1550; $i++)
+{
+    echo "<h1>".$i."<br>";
+}
+//echo "<script>window.location.href = 'http//www.globo.com</script>'"
 
 
 
