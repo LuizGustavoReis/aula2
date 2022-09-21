97d59dbee8af7988f635a74c0041ee0bbe05c681
LuizGustavoReis <luizgusreis@gmail.com>
2022-09-21 11:03:13 -0300

    fazendo a pesquisa

--- diciplinas.php
+++ diciplinas.php
@@ -2,10 +2,16 @@
 
 
 <?php
+if(isset($_GET["busca"] ) && !empty($_GET["busca"])){
+    $dados = file_get_contents("https://reserva.fatectq.edu.br/API/DISCIPLINAS/busca/".$_GET["busca"]);
+    $dados = json_decode($dados, true);
+
+}else{
 //pegando informaçoes da API
 $dados = file_get_contents("https://reserva.fatectq.edu.br/API/DISCIPLINAS");
 //corvertendo as imformaçoes 
 $dados = json_decode($dados, true);
+}
 
 if(isset($_GET["erro"])&& $_GET["erro"]=="semid"){
     ?>
@@ -21,7 +27,31 @@ if(isset($_GET["erro"])&& $_GET["erro"]=="semid"){
 
 
 <div class="row">
-    <div class="col-8 offset-2">
+    <div class="col-10offset-1">
+
+      <div class="card">
+          <div class="card-header">Lista de disciplina</div>
+          <div class= "card-body">
+          <form action="./diciplinas.php" method="get">
+              <div class="row">
+                 
+                  <div class="col-3">
+                      <a class="btn btn-succes">
+                          nova Diciplina
+                      </a>
+                  </div>
+                  <div class="col-4">
+                      <input type="text" name="busca" class="form-control"/>
+                  </div>
+                  <div class="col-2">
+                      <button type="submit" class="btn btn-primary">pesquisar</button>
+                  </div>
+              </div>
+                  </form>
+              </div>
+          </div>
+
+      </div>
         <table class="table table-hover table-striped">
 
             <thead>
